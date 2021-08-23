5844d1223f12178f9976130bb89101fe335a7a24
Peter <petermeinder@gmail.com>
2021-08-23 10:14:08 -0400

    Only show minimize on folders. Icon change.

--- webviews/components/CodeMap/Card.svelte
+++ webviews/components/CodeMap/Card.svelte
@@ -4,7 +4,7 @@
   import deepdash from "deepdash";
   import { afterUpdate } from "svelte";
   import Fa from "svelte-fa";
-  import { faBullseye, faCompressArrowsAlt, faFile, faFolder, faTrashRestore } from "@fortawesome/free-solid-svg-icons";
+  import { faBullseye, faCompressArrowsAlt, faExpandAlt, faFile, faFolder, faTrashRestore } from "@fortawesome/free-solid-svg-icons";
 
   const _ = deepdash(lodash);
   export let left = 30;
@@ -17,7 +17,6 @@
   export let SelectPerimeter = () => {};
 
   export let Minimize = (event, treeItem) => {};
-
 
   afterUpdate(() => {
     $newRender = $newRender++;
@@ -33,10 +32,7 @@
     };
   }
 
-
-
-
-  function dbClick(path:string) {
+  function dbClick(path: string) {
     tsvscode.postMessage({
       type: "OpenFile",
       value: path,
@@ -69,8 +65,18 @@
     <Fa size="1x" icon={faFile} style="color:white; padding-right: 4px; padding-left:4px; float:right" />
   {/if}
   <button id="SelectPerimeter" on:click={GroupClick}><Fa size="1x" icon={faBullseye} style="color:red; padding-right: 4px; float:right" /></button>
-  <button id="Minimize" on:click={(event) => Minimize(event, treeItem)}><Fa size="1x" icon={faCompressArrowsAlt} style="color:red; padding-right: 4px; float:right" /></button>
 
+  {#if treeItem.type === "directory"}
+    {#if typeof(treeItem.open) === 'undefined' || treeItem.open === true}
+      <button id="Minimize" on:click={(event) => Minimize(event, treeItem)}>
+        <Fa size="1x" icon={faCompressArrowsAlt} style="color:yellow; padding-right: 4px; float:right" />
+      </button>
+      {:else}
+      <button id="Minimize" on:click={(event) => Minimize(event, treeItem)}>
+        <Fa size="1x" icon={faExpandAlt} style="color:yellow; padding-right: 4px; float:right" />
+      </button>
+    {/if}
+  {/if}
   <button type="button" class="inner-hide"> {treeItem.name}</button>
 </main>
 
